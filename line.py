069fb13435b424ba5735a1d22f8f726b249b58c1
dongrenguang <dongrenguang@baidu.com>
2017-08-19 22:24:03 +0800

    vector and line

---
+++ line.py
@@ -0,0 +1,176 @@
+# coding=utf-8
+
+from decimal import Decimal, getcontext
+from vector import Vector
+
+getcontext().prec = 30
+
+
+class Line(object):
+
+    NO_NONZERO_ELTS_FOUND_MSG = 'No nonzero elements found'
+
+    def __init__(self, normal_vector=None, constant_term=None):
+        self.dimension = 2
+
+        if not normal_vector:
+            all_zeros = ['0']*self.dimension
+            normal_vector = Vector(all_zeros)
+        self.normal_vector = normal_vector
+
+        if not constant_term:
+            constant_term = Decimal('0')
+        self.constant_term = Decimal(constant_term)
+
+        self.set_basepoint()
+
+
+    # 基点，即(0, b) or (a, 0)
+    def set_basepoint(self):
+        try:
+            n = self.normal_vector.coordinates
+            c = self.constant_term
+            basepoint_coords = ['0']*self.dimension
+
+            initial_index = Line.first_nonzero_index(n)
+            initial_coefficient = n[initial_index]
+
+            basepoint_coords[initial_index] = c/initial_coefficient
+            self.basepoint = Vector(basepoint_coords)
+
+        except Exception as e:
+            if str(e) == Line.NO_NONZERO_ELTS_FOUND_MSG:
+                self.basepoint = None
+            else:
+                raise e
+
+
+    # 是否平行
+    def is_parallel_to(self, line2):
+        return self.normal_vector.is_parallel_to(line2.normal_vector);
+
+
+    # 是否是同一条直线
+    def is_same(self, line2):
+        if self.normal_vector.is_zero():
+            if not line2.normal_vector.is_zero():
+                return False
+            else:
+                diff = self.constant_term - line2.constant_term
+                return MyDecimal(diff).is_near_zero()
+        elif line2.normal_vector.is_zero():
+            return False
+
+        if (not self.is_parallel_to(line2)):
+            return False;
+        # n1 = self.normal_vector.coordinates
+        # n2 = line2.normal_vector.coordinates
+        # c1 = self.constant_term
+        # c2 = line2.constant_term
+        # initial_index1 = Line.first_nonzero_index(n1)
+        # initial_index2 = Line.first_nonzero_index(n2)
+        # initial_coefficient1 = n1[initial_index1]
+        # initial_coefficient2 = n2[initial_index2]
+        # delta = initial_coefficient1 / initial_coefficient2 - self.constant_term / line2.constant_term
+        # return MyDecimal(delta).is_near_zero()
+
+        x0 = self.basepoint
+        y0 = line2.basepoint
+        basepoint_difference = x0.minus(y0)
+        n = self.normal_vector
+        return n.is_orthogonal_to(basepoint_difference)
+
+
+    # 交点
+    def intersection_with(self, line2):
+        if (self.is_same(line2)):
+            print 'They are the same line, so they have countless intersection.'
+        elif (self.is_parallel_to(line2)):
+            print 'They are parallel, but not the same, so they have no intersection.'
+        else:
+            A = self.normal_vector.coordinates[0]
+            B = self.normal_vector.coordinates[1]
+            k1 = self.constant_term
+            C = line2.normal_vector.coordinates[0]
+            D = line2.normal_vector.coordinates[1]
+            k2 = line2.constant_term
+            x = (D * k1 - B * k2) / (A * D - B * C)
+            y = (-C * k1 + A * k2) / (A * D - B * C)
+            return [x, y]
+
+
+    def __str__(self):
+
+        num_decimal_places = 3
+
+        def write_coefficient(coefficient, is_initial_term=False):
+            coefficient = round(coefficient, num_decimal_places)
+            if coefficient % 1 == 0:
+                coefficient = int(coefficient)
+
+            output = ''
+
+            if coefficient < 0:
+                output += '-'
+            if coefficient > 0 and not is_initial_term:
+                output += '+'
+
+            if not is_initial_term:
+                output += ' '
+
+            if abs(coefficient) != 1:
+                output += '{}'.format(abs(coefficient))
+
+            return output
+
+        n = self.normal_vector.coordinates
+
+        try:
+            initial_index = Line.first_nonzero_index(n)
+            terms = [write_coefficient(n[i], is_initial_term=(i==initial_index)) + 'x_{}'.format(i+1)
+                     for i in range(self.dimension) if round(n[i], num_decimal_places) != 0]
+            output = ' '.join(terms)
+
+        except Exception as e:
+            if str(e) == self.NO_NONZERO_ELTS_FOUND_MSG:
+                output = '0'
+            else:
+                raise e
+
+        constant = round(self.constant_term, num_decimal_places)
+        if constant % 1 == 0:
+            constant = int(constant)
+        output += ' = {}'.format(constant)
+
+        return output
+
+
+    # 第一个不为0的位置
+    @staticmethod
+    def first_nonzero_index(iterable):
+        for k, item in enumerate(iterable):
+            if not MyDecimal(item).is_near_zero():
+                return k
+        raise Exception(Line.NO_NONZERO_ELTS_FOUND_MSG)
+
+
+class MyDecimal(Decimal):
+    def is_near_zero(self, eps=1e-10):
+        return abs(self) < eps
+
+
+# line1 = Line(Vector([4.046, 2.836]), 1.21)
+# line2 = Line(Vector([10.115, 7.09]), 3.025)
+
+# line1 = Line(Vector([7.204, 3.182]), 8.68)
+# line2 = Line(Vector([8.172, 4.114]), 9.883)
+
+# line1 = Line(Vector([1.182, 5.562]), 6.744)
+# line2 = Line(Vector([1.773, 8.343]), 9.525)
+
+line1 = Line(Vector([0, 0]), 1.21)
+line2 = Line(Vector([0, 0]), 1.21)
+
+print line1.intersection_with(line2)
+print line1.is_parallel_to(line2)
+print line1.is_same(line2)
---
+++ vector.py
@@ -0,0 +1,153 @@
+# coding=utf-8
+
+from math import sqrt, acos, pi
+from decimal import Decimal, getcontext
+
+getcontext().prec = 30
+
+class Vector(object):
+
+    CANNOT_NORMALIZE_ZEOR_VECTOR_MSG = 'Cannot normalize the zero vector'
+
+    def __init__(self, coordinates):
+        try:
+            if not coordinates:
+                raise ValueError
+            self.coordinates = tuple([Decimal(x) for x in coordinates])
+            self.dimension = len(self.coordinates)
+
+        except ValueError:
+            raise ValueError('The coordinates must be nonempty')
+
+        except TypeError:
+            raise TypeError('The coordinates must be an iterable')
+
+
+    def plus(self, v):
+        new_coordinates = [x+y for x,y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
+
+
+    def minus(self, v):
+        new_coordinates = [x-y for x,y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
+
+
+    def time_scalar(self, c):
+        new_coordinates = [Decimal(str(c))*x for x in self.coordinates]
+        return Vector(new_coordinates)
+
+
+    # 长度
+    def magnitude(self):
+        coordinates_squared = [x**2 for x in self.coordinates]
+        return Decimal(sqrt(sum(coordinates_squared)))
+
+
+    # 单位化（即长度为1）
+    def normalized(self):
+        try:
+            magnitude = self.magnitude()
+            return self.time_scalar(Decimal('1.0')/magnitude)
+        except ZeroDivisionError:
+            raise Exception(CANNOT_NORMALIZE_ZEOR_VECTOR_MSG)
+
+
+    # 内积（是一个值）
+    def dot(self, v):
+        return sum([x*y for x,y in zip(self.coordinates, v.coordinates)])
+
+
+    # 夹角
+    def angle_with(self, v, in_degrees=False):
+        try:
+            u1 = self.normalized()
+            u2 = v.normalized()
+            angle_in_radians = Decimal(acos(u1.dot(u2)))
+
+            if in_degrees:
+                return angle_in_radians * (Decimal('180.0') / Decimal(pi))
+            else:
+                return angle_in_radians
+
+        except Exception as e:
+            if str(e) == self.CANNOT_NORMALIZE_ZEOR_VECTOR_MSG:
+                raise Exception(CANNOT_NORMALIZE_ZEOR_VECTOR_MSG)
+            else:
+                raise e
+
+
+    # 是否正交
+    def is_orthogonal_to(self, v, tolerance=1e-10):
+        return abs(self.dot(v)) < tolerance
+
+
+    # 是否平行
+    def is_parallel_to(self, v, tolerance=1e-10):
+        return self.is_zero() or v.is_zero() or self.angle_with(v) < Decimal(tolerance) or abs(self.angle_with(v) - Decimal(pi)) < Decimal(tolerance)
+
+
+    # 是否是0向量
+    def is_zero(self, tolerance=1e-10):
+        return self.magnitude() < tolerance
+
+
+    def component_parallel_to(self, b):
+        try:
+            u = b.normalized()
+            return u.time_scalar(self.dot(u))
+        except Exception as e:
+            raise e
+
+
+    def component_orthogonal_to(self, b):
+        try:
+            return self.minus(self.component_parallel_to(b))
+        except Exception as e:
+            raise e
+
+
+    # 向量积（是一个向量）
+    def cross(self, v):
+        if (len(v.coordinates) > 3):
+            return;
+        x1 = self.coordinates[0]
+        y1 = self.coordinates[1]
+        z1 = self.coordinates[2]
+        x2 = v.coordinates[0]
+        y2 = v.coordinates[1]
+        z2 = v.coordinates[2]
+        return Vector([y1*z2-y2*z1, x2*z1-x1*z2, x1*y2-x2*y1])
+
+
+    # 两个向量为边组成的平行四边形的面积
+    def area(self, v):
+        return self.cross(v).magnitude()
+
+
+    # 两个向量为边组成的三角形的面积
+    def helf_area(self, v):
+        return self.area(v) / Decimal('2.0')
+
+
+    def __str__(self):
+        return 'Vector: {}'.format(self.coordinates)
+
+
+    def __eq__(self, v):
+        return self.coordinates == v.coordinates
+
+
+
+# v = Vector(['8.462', '7.893', '-8.187'])
+# w = Vector(['6.984', '-5.975', '4.778'])
+# print v.cross(w)
+#
+# v = Vector(['-8.987', '-9.838', '5.031'])
+# w = Vector(['-4.268', '-1.861', '-8.866'])
+# print v.area(w)
+#
+# v = Vector([pi, '9.547', '3.691'])
+# w = Vector(['-6.007', '0.124', '5.772'])
+# print v.cross(w)
+# print v.helf_area(w)
